29b95b4eb07705e4a1b29ce0266fd3e8765cacf9
Salman <salmanmaken57@gmail.com>
2025-11-05 20:55:00 +0500

    Features: DB(PostgreSQL), Frontend(UI and Buttons enhanced)

--- notes_app/notes/models.py
+++ notes_app/notes/models.py
@@ -9,4 +9,4 @@ class Notes(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.title   
--- notes_app/notes_app/settings.py
+++ notes_app/notes_app/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'notes_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notes_db',
+        'USER': 'notes_user',
+        'PASSWORD': 'pythondev',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
